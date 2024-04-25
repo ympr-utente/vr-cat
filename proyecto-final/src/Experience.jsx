7a45ffcdc455986d3671b35739e86bd3b9ad0303
JoseManuel2005 <jose.palma@correounivalle.edu.co>
2024-04-24 22:44:13 -0500

    Nivel con el piso modelado, falta insertar modelos

---
+++ proyecto-final/src/Experience.jsx
@@ -0,0 +1,17 @@
+import { MeshBasicMaterial } from "three";
+import { OrbitControls } from "@react-three/drei";
+import World from "./World/World.jsx";
+
+const Experience    = ({title,subtitle}) => {
+    return (
+        
+       <mesh position-x={20} rotation-y={-Math.PI * 0.25} scale={4}>
+        <OrbitControls />
+        <ambientLight intensity={0.5}></ambientLight>
+        <directionalLight position={[10,10,5]} intensity={2}></directionalLight>    
+        <World/>
+       </mesh>
+    );
+}
+
+export default Experience;
---
+++ proyecto-final/src/World/World.jsx
@@ -0,0 +1,64 @@
+import { useGLTF, useTexture } from "@react-three/drei";
+import { RepeatWrapping } from "three";
+
+export default function World(props) {   
+    const {nodes,materials} = useGLTF('/assets/models/world/World.glb');
+    const PATH = '/assets/textures/floor/';   
+    //const PATH = '/assets/textures/prueba/';   
+    /*const propsTextures = useTexture(
+        {   
+        map: PATH + 'asphalt_02_diff_1k.jpg',
+        displacementMap: PATH + 'asphalt_02_disp_1k.png',
+        normalMap: PATH + 'asphalt_02_nor_gl_1k.jpg',
+        roughnessMap: PATH + 'asphalt_02_rough_1k.jpg',
+        }
+    );
+*/
+    const propsTextures = useTexture(
+      {   
+      map: PATH + 'pavement_02_diff_1k.jpg',
+      displacementMap: PATH + 'pavement_02_disp_1k.png',
+      normalMap: PATH + 'pavement_02_nor_gl_1k.jpg',
+      roughnessMap: PATH + 'pavement_02_rough_1k.jpg',
+      }
+  );
+
+
+    propsTextures.map.repeat.set(10,74);
+    propsTextures.map.wrapS = RepeatWrapping;
+    propsTextures.map.wrapT = RepeatWrapping;
+
+    propsTextures.displacementMap.repeat.set(10,74);
+    propsTextures.displacementMap.wrapS = RepeatWrapping;
+    propsTextures.displacementMap.wrapT = RepeatWrapping;
+
+    propsTextures.normalMap.repeat.set(10,74);
+    propsTextures.normalMap.wrapS = RepeatWrapping;
+    propsTextures.normalMap.wrapT = RepeatWrapping;
+
+    propsTextures.roughnessMap.repeat.set(10,74);
+    propsTextures.roughnessMap.wrapS = RepeatWrapping;
+    propsTextures.roughnessMap.wrapT = RepeatWrapping;
+
+    /*const propsTextures = useTexture(
+        {   
+        map: PATH + 'coast_sand_01_diff_1k.jpg',
+        displacementMap: PATH + 'coast_sand_01_disp_1k.png',
+        normalMap: PATH + 'coast_sand_01_nor_gl_1k.jpg',
+        roughnessMap: PATH + 'coast_sand_01_rough_1k.jpg',
+        }
+    );*/
+    console.log(propsTextures);
+
+    return(
+        <group {...props} dispose={null}>
+        <group>
+          <mesh geometry={nodes.Floor.geometry} material={materials['Material.001']}>
+            <meshStandardMaterial {...propsTextures} />
+          </mesh>
+        </group>
+      </group>
+    )
+    }
+
+useGLTF.preload('/assets/models/world/World.glb');
