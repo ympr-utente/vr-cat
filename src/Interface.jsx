09338217c250d2632ff84d3bfca95ecbf18665ce
Yampier <yampier.email@gmail.com>
2024-05-03 16:37:44 -0500

    Implementar guias de movimiento de botones (flechas y espacio)

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,4 +1,4 @@
-import { useKeyboardControls} from '@react-three/drei'
+import { useKeyboardControls } from '@react-three/drei'
 // import { addEffect } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import UseAnimations from 'react-useanimations'
@@ -19,6 +19,25 @@ function Interface() {
 
     const controls = useKeyboardControls((state) => state)
 
+    // useEffect(() => {
+    //     return addEffect(() => {
+    //         const state = useGame.getState()
+
+    //         let elapsedTime = 0
+
+    //         if (state.phase === 'playing') {
+    //             elapsedTime = Date.now() - state.startTime
+    //         } else if (state.phase === 'ended') {
+    //             elapsedTime = state.endTime - state.startTime
+    //         }
+
+    //         elapsedTime /= 1000
+    //         elapsedTime = elapsedTime.toFixed(2)
+
+    //         if (timerRef.current) timerRef.current.innerText = elapsedTime
+    //     })
+    // }, [])
+
     function handleToggleAudio(e) {
         toggleAudio()
         e.target.blur()
@@ -26,17 +45,32 @@ function Interface() {
 
     return (
         <div className="interface">
+            {highScore > 0 && (
+                <div className="high-score">
+                    <h2>High Score: {(highScore / 1000).toFixed(2)}</h2>
+                </div>
+            )}
+
             <button className="audio-toggle" onClick={handleToggleAudio}>
                 <UseAnimations animation={volume} reverse={!audio} strokeColor="white" />
             </button>
+                        <div className="controls">
+                <div className="raw">
+                    <div className={`key ${controls.forward ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key ${controls.left ? 'active' : ''}`}></div>
+                    <div className={`key ${controls.backward ? 'active' : ''}`}></div>
+                    <div className={`key ${controls.right ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key large ${controls.jump ? 'active' : ''}`}></div>
+                </div>
+            </div>
 
-            {gamePhase === 'ready' && (
-                <h2 className="cta" onClick={startGame}>
-                    Play
-                </h2>
-            )}
-        </div>
+            </div>
     )
 }
 
 export { Interface }
+
