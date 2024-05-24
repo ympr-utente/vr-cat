391180407bfe51ee96acd882fd506dc57f1bbd27
Yampier <yampier.email@gmail.com>
2024-05-24 15:53:26 -0500

    Agregar ruta de nivel 2

---
+++ src/pages/level2/trophy/Trophy.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useMemo, useEffect } from 'react'
+import { Float, useGLTF } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
+import { useGame } from '../../../stores/useGame'
+import { useAudio } from '../../../stores/useAudio'
+
+export default function Trophy(props) {
+    const group = useRef()
+    const { nodes, materials } = useGLTF('./assets/plato-gato/plato-gato.glb')
+
+    const endGame = useGame((state) => state.end)
+    const audio = useAudio((state) => state.audio)
+    const hitSound = useMemo(() => new Audio('./assets/sounds/hit.mp3'), [])
+
+    function onHit({ other }) {
+        hitSound.currentTime = 0
+        hitSound.volume = Math.random() * 0.1
+        hitSound.play()
+    }
+
+    useEffect(() => {
+        hitSound.muted = !audio
+    }, [audio])
+
+    return (
+        <RigidBody type="fixed" colliders="hull" restitution={0.2} friction={0} onCollisionEnter={onHit} {...props}>
+            <Float speed={2} rotationIntensity={0.1} floatIntensity={1}>
+            <group {...props} dispose={null}>
+            <group name="Scene">
+                <mesh
+                name="Orejas"
+                castShadow
+                receiveShadow
+                geometry={nodes.Orejas.geometry}
+                material={materials['Material.002']}
+                position={[-4.077, 7.675, -2.887]}
+                rotation={[2.961, 0.01, 2.433]}
+                scale={[1.027, 1.027, 0.653]}
+                />
+                <mesh
+                name="Círculo"
+                castShadow
+                receiveShadow
+                geometry={nodes.Círculo.geometry}
+                material={materials['Material.002']}
+                position={[0.003, 4.826, -2.624]}
+                rotation={[-1.887, -0.001, -3.138]}
+                scale={-5.056}
+                />
+            </group>
+            </group>
+            </Float>
+        </RigidBody>
+    )
+}
+
+useGLTF.preload('./assets/plato-gato/plato-gato.glb')
--- src/routes/RoutesGame.jsx
+++ src/routes/RoutesGame.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Level1 from "../pages/level1/Level1";
+import Level2 from "../pages/level2/Level2";
 import Login from "../pages/login/Login";
 import Home from "../pages/main-view/Home";
 
@@ -10,8 +11,8 @@ export default function RoutesGames() {
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="/level1" element={<Level1/>} />
-                {/* <Route path="/level2" element={<Level1 />} />
-                <Route path="/level3" element={<Level1 />} />
+                <Route path="/level2" element={<Level2/>} />
+                {/* <Route path="/level3" element={<Level1 />} />
                 <Route path="/level4" element={<Level1 />} /> */}
             </Routes>
         </BrowserRouter>
