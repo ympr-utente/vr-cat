211e9e3d844e74c6e6301f67ccc751b710a462f0
AlejoZA <117043041+AlejoZA@users.noreply.github.com>
2024-05-25 02:39:22 -0500

    Update Login.jsx
    Signed-off-by: AlejoZA <117043041+AlejoZA@users.noreply.github.com>

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,51 +1,73 @@
-// src/context/AuthContext.jsx
-import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
-import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from '../stores/firebase.config';
+// import React from 'react';
+// import './StylesLogin.css';
+// import { useNavigate } from 'react-router-dom';
 
-export const authContext = createContext();
 
-export const useAuth = () => {
-    const context = useContext(authContext);
-    if (!context) {
-        console.error("useAuth must be used within an AuthProvider");
-        return;
+// export default function Login() {
+
+//     return (
+//         <div className='container'>
+//             <div className='wrapper'>
+//                 <form action=''>
+//                     <div className="logo-meow">
+//                         <img src="/assets/images/logo.png" alt="Logo" />
+
+//                     </div>
+//                     <div className='title-login'>
+//                         <h1>MeowVr</h1>
+//                     </div>
+//                     <div className='google-button'>
+//                         <button type='submit'>Login</button>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+import React from 'react';
+import './StylesLogin.css';
+import { useNavigate } from 'react-router-dom';
+import { useAuth, loginWithGoogle } from '../../context/AuthContext';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+export default function Login() {
+    const navigate = useNavigate();
+    //useAuth();
+    const auth = useAuth();
+
+    const onHandleButtonLogin = async (e) => {
+        e.preventDefault()
+        // const result = 
+        const result = await auth.loginWithGoogle()
+            .then((res) => {
+                // console.log(result),
+                navigate('/level1')
+            })
+            .catch((error) => console.error(error));
     }
-    return context;
-}
 
-export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    useEffect(() => {
-        const subscribed = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser || null);
-        });
-        return () => subscribed();
-    }, []);
-
-    const loginWithGoogle = async () => {
-        try {
-            const provider = new GoogleAuthProvider();
-            const res = await signInWithPopup(auth, provider);
-            return { success: true, user: res.user };
-        } catch (error) {
-            console.error('Error al iniciar sesión con Google:', error);
-            return { success: false, error: error };
-        }
-    }
-
-    const logout = async () => {
-        try {
-            await signOut(auth);
-            return { success: true };
-        } catch (error) {
-            return { success: false, error: error };
-        }
-    };
 
     return (
-        <authContext.Provider value={{ user, loginWithGoogle, logout }}>
-            {children}
-        </authContext.Provider>
+        <div className='container'>
+            <div className='wrapper'>
+                <form action=''>
+                    <div className="logo-meow">
+                        <img src="/assets/images/threedy-logo.svg" alt="Logo" />
+
+                    </div>
+                    <div className='title-login'>
+                        <h1>MeowVr</h1>
+                    </div>
+                    <div onClick={onHandleButtonLogin} className="Button-Start">
+                        <button>Login with Google</button>
+                    </div>
+                    <div className='guest-button'>
+                        <button type='submit'>Guest</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
