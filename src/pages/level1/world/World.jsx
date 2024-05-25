7b3190ed6b6a7a78fa62ce025cbdf9794adff3f8
AlejoZA <117043041+AlejoZA@users.noreply.github.com>
2024-05-25 13:15:26 -0500

    Update World.jsx
    Signed-off-by: AlejoZA <117043041+AlejoZA@users.noreply.github.com>

--- src/pages/level1/world/World.jsx
+++ src/pages/level1/world/World.jsx
@@ -1,178 +1,41 @@
-// src/pages/level2/world/World.jsx
-import { Environment, KeyboardControls } from '@react-three/drei'
-import { RigidBody } from '@react-three/rapier'
-import Ecctrl, { EcctrlAnimation } from 'ecctrl'
-import { useEffect, useMemo, useRef, useState } from 'react'
-import CatModel from '../../../components/characters/CatModel'
-import Obstacle from '../../../components/obstacles/Obstacle'
-import Castillo from '../../../components/obstacles/nivel2/Castillo'
-import { Fish } from '../../../components/rewards/Fish'
-import { useAuth } from '../../../context/AuthContext'; // Ruta ajustada
-import { saveCheckpoint } from '../../../stores/saveCheckpoint';
-import { useGame } from '../../../stores/useGame'
-import Spinner from '../../../components/obstacles/Spinner'
-import Floor from '../floor/Floor'
-import Trophy from '../trophy/Trophy'
-import Boxer from '../Boxer/Boxer'
+import { Canvas } from '@react-three/fiber';
+import { Physics } from '@react-three/rapier';
+import React, { Suspense } from 'react';
+import { GameInterface } from '../../GameInterface';
+import { Title } from '../../components/titles/Title';
+import { CatModelProvider } from '../../context/CatModelContext';
+import { ShortcutManager } from '../../managers/ShortcutManager';
+import { SoundManager } from '../../managers/SoundManager';
+import Lights from '../../pages/level1/lights/Lights';
+import { NavigationControls } from '../../utils/NavigationControls';
+import World from './world/World';
 
-export default function World() {
-    const { user } = useAuth();
-    const characterURL = "./assets/character/threedy-realease.glb";
-  
-    const keyboardMap = [
-        { name: "forward", keys: ["ArrowUp", "KeyW"] },
-        { name: "backward", keys: ["ArrowDown", "KeyS"] },
-        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
-        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
-        { name: "jump", keys: ["Space"] },
-        { name: "run", keys: ["Shift"] },
-        { name: "action1", keys: ["1"] },
-        { name: "action2", keys: ["2"] },
-        { name: "action3", keys: ["3"] },
-        { name: "action4", keys: ["KeyF"] },
-    ];
 
-    const animationSet = {
-        idle: "Listisimo",
-        walk: "CaminarConEstilacho",
-        run: "CorriendoALoMazeRunner",
-        jump: "SaltadorIntrepido",  
-        jumpIdle: "AterrizandoConParkour", 
-        jumpLand: "AterrizandoConParkour", 
-        fall: "Paracaidista", 
-        action1: "Izquierdazo",
-        action2: "Derechazo",
-        action3: "PatadaVoladora",
-    };
+export default function Level1() {
+  return (
+    <CatModelProvider>
+      <NavigationControls>
+        <Canvas
+          shadows
+          camera={{
+            position: [0, 5, 10]
+          }}>
+          <color attach="background" args={['#FEF9F7']} />
 
-    const [score, setScore] = useState(0);
+          <Lights />
 
-    const gameStarted = useGame((state) => state.gameStarted);
-    const restartGame = useGame((state) => state.restart);
-    const fishes = useGame((state) => state.fishes);
-    const updateCatPosition = useGame((state) => state.updateCatPosition);
-    const catPosition = useGame((state) => state.catPosition);
-    const resetBonusVisible = useGame((state) => state.resetBonusVisible);
-    const bonusVisible = useGame((state) => state.bonusVisible);
-    const setNotification = useGame((state) => state.setNotification);
-    const saveCheckpointState = useGame((state) => state.saveCheckpoint);
-
-    const catRef = useRef(); // Referencia para el modelo del gato
-
-    const successSound = useMemo(() => {
-        const sound = new Audio('./assets/sounds/+5.mp3')
-        sound.volume = 0.2
-        return sound
-    }, [])
-
-    const onEatFish = (id) => {
-        setScore(score + 1);
-        useGame.setState((state) => ({
-            fishes: state.fishes.filter((fish) => fish.id !== id)
-        }));
-        useGame.getState().addTime(); // Añadir 5 segundos al temporizador
-        successSound.play(); // Reproducir sonido de "+5"
-    }
-
-    const onContactYellowSquare = async () => {
-        if (catRef.current && user) {
-            const catObject = catRef.current;
-            const catPosition = new THREE.Vector3();
-            catObject.getWorldPosition(catPosition);
-
-            console.log(`Cat Position when contacting yellow square:`, { x: catPosition.x, y: catPosition.y, z: catPosition.z });
-
-            const catPos = { x: catPosition.x, y: catPosition.y, z: catPosition.z };
-            const countdown = useGame.getState().countdown;
-            const fishes = useGame.getState().fishes;
-
-            saveCheckpointState(catPos, countdown, fishes);
-            setNotification('Checkpoint saved!');
-            await saveCheckpoint(user.uid, catPos, countdown, fishes);
-        } else {
-            console.error("Cat ref is not defined or user is not logged in");
-        }
-    };
-
-    // Resetear la visibilidad del "+5" después de 1 segundo
-    useEffect(() => {
-        if (bonusVisible) {
-            const timer = setTimeout(() => {
-                resetBonusVisible();
-            }, 1000);
-
-            return () => clearTimeout(timer);
-        }
-    }, [bonusVisible, resetBonusVisible]);
-
-    // Resetear la posición del gato cuando se reinicia el juego
-    useEffect(() => {
-        if (catRef.current) {
-            catRef.current.position.set(catPosition.x, catPosition.y, catPosition.z); // Restablecer la posición inicial del gato
-        }
-    }, [gameStarted, catPosition]);
-
-    return (
-        <>
-            <Environment
-                files={"./assets/hdris/belfast_sunset_puresky_4k.hdr"}
-                background = {true}
-                ground={{height: 20, scale: 512, radius: 400}}
-            />
-            <KeyboardControls map={keyboardMap}>
-                <Ecctrl animated={true}
-                    camInitDis={-8}
-                    camMaxDis={-8}
-                    maxVelLimit={gameStarted ? 5 : 0} // Deshabilitar el movimiento si el juego no ha comenzado
-                    jumpVel={gameStarted ? 6 : 0} // Deshabilitar el salto si el juego no ha comenzado
-                    position={[0, 40, 0]}
-                >
-                    <EcctrlAnimation
-                        characterURL={characterURL}
-                        animationSet={animationSet}
-                    >
-                        <CatModel ref={catRef} />
-                    </EcctrlAnimation>
-                </Ecctrl>
-            </KeyboardControls>
-             
-            {fishes.map((fish) => (
-                <RigidBody scale={0.7} key={fish.id} type='fixed' colliders={"hull"} onCollisionEnter={() => onEatFish(fish.id)}>
-                    <Fish 
-                        position={fish.position}
-                    />
-                </RigidBody>
-            ))}
-
-            {/* el coso del checkpoint */}
-            <RigidBody
-                scale={0.7}
-                type="fixed"
-                colliders={"hull"}
-                onCollisionEnter={onContactYellowSquare}
-                position={[0, 2, -35]} 
-            >
-                <mesh>
-                    <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="yellow" />
-                </mesh>
-            </RigidBody>
-
-            <Floor scale-y={5} position-z={-45} />
-
-            <Spinner position={[-8, 4, -26]} speed={8}/>
-            <Spinner position={[8, 4, -16]} speed={4} initialShift={1}/>
-
-            <Obstacle color='white' position-z={-4} />
-            <Obstacle.SlidingWall color='white' position-z={-45} />
-            <Obstacle.Spinner color='white' position-z={-32} speed={5} />
-            <Obstacle.Spinner color='white' position-z={-52} speed={5} position-x={6} scale-x={0.75} />
-            <Obstacle.Spinner color='white' position-z={-52} speed={5} position-x={-6} scale-x={0.75} invert />
-           
-            <Boxer position={[-0.5,2.15,-37.6]} rotation-y={0} scale={35}/>
-            
-            <Trophy position-z={-45} position-y={1}/>
-
-        </>
-    )
+          <Suspense fallback={null}>
+            <Physics>
+              <Title />
+              <World />
+              <ShortcutManager />
+              <SoundManager />
+            </Physics>
+          </Suspense>
+        </Canvas>
+        <GameInterface />
+        <ShortcutManager />
+      </NavigationControls>
+    </CatModelProvider>
+  );
 }
