657d0e457499a6cb40f0d21e750a954d9d4d3380
Yampier <yampier.email@gmail.com>
2024-05-25 01:43:50 -0500

    Agregar Piso y Texturas PBR

--- src/pages/level2/floor/Floor.jsx
+++ src/pages/level2/floor/Floor.jsx
@@ -1,20 +1,32 @@
+import React from 'react';
 import { RigidBody } from '@react-three/rapier';
-import { MeshStandardMaterial, TextureLoader } from 'three';
+import { TextureLoader } from 'three';
+import { useLoader } from '@react-three/fiber';
 
-export default function Floor(props) {
-    // Carga de las texturas
-    const textureLoader = new TextureLoader();
-    const textureColor = textureLoader.load('./assets/floor/oakfloor_basecolor.png');
-    const textureRoughness = textureLoader.load('./assets/floor/oakfloor_roughness.png');
-    const textureMetalness = textureLoader.load('./assets/floor/oakfloor_Height.png');  
+export default function SandFloor(props) {
+    // Cargar las texturas
+    const [albedoMap, aoMap, heightMap, metallicMap, normalMap, roughnessMap] = useLoader(TextureLoader, [
+        './assets/floor-sand/wavy-sand_albedo.png',
+        './assets/floor-sand/wavy-sand_ao.png',
+        './assets/floor-sand/wavy-sand_height.png',
+        './assets/floor-sand/wavy-sand_metallic.png',
+        './assets/floor-sand/wavy-sand_normal-dx.png',
+        './assets/floor-sand/wavy-sand_roughness.png'
+    ]);
 
     return (
-        <RigidBody type="fixed" rotation-x={Math.PI * -0.5} restitution={0.2} friction={4} {...props}> <mesh receiveShadow> <boxGeometry args={[20, 20, 1]} /> <meshStandardMaterial
-                    map={textureColor}
-                    roughnessMap={textureRoughness}
-                    metalnessMap={textureMetalness}
+        <RigidBody type="fixed" rotation-x={Math.PI * -0.5} restitution={0.2} friction={4} {...props}>
+            <mesh receiveShadow>
+                <boxGeometry args={[20, 20, 1]} />
+                <meshStandardMaterial
+                    map={albedoMap}
+                    aoMap={aoMap}
+                    displacementMap={heightMap}
+                    metalnessMap={metallicMap}
+                    normalMap={normalMap}
+                    roughnessMap={roughnessMap}
                 />
             </mesh>
         </RigidBody>
-    )
+    );
 }
