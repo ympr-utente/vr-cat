9297c75fa903537a615e67c43c630adf5c9384e6
Jose Manuel Palma Oquendo <80994908+JoseManuel2005@users.noreply.github.com>
2024-05-30 14:53:23 -0500

    empezando nivel 3

---
+++ src/pages/level3/Level3.jsx
@@ -0,0 +1,73 @@
+import { Canvas } from '@react-three/fiber';
+import { Physics } from '@react-three/rapier';
+import React, { Suspense } from 'react';
+import { GameInterface } from '../../GameInterface';
+import { Title } from '../../components/titles/Title';
+import { CatModelProvider } from '../../context/CatModelContext';
+import { ShortcutManager } from '../../managers/ShortcutManager';
+import { SoundManager } from '../../managers/SoundManager';
+import Lights from '../level1/lights/Lights';
+import { NavigationControls } from '../../utils/NavigationControls';
+import World from './world/World';
+
+export default function level3() {
+  return (
+    // <CatModelProvider>
+    //   <NavigationControls>
+        <Canvas
+          shadows
+          camera={{
+            position: [0, 5, 10]
+          }}>
+          <color attach="background" args={['#FEF9F7']} />
+
+          {/* <Lights /> */}
+
+          <Suspense fallback={null}>
+            <Physics>
+              {/* <Title /> */}
+              <World />
+              {/* <ShortcutManager />
+              <SoundManager /> */}
+            </Physics>
+          </Suspense>
+        </Canvas>
+    //      <GameInterface />
+    //     <ShortcutManager />
+    //   </NavigationControls>
+    // </CatModelProvider> 
+  );
+}
+
+
+
+
+
+// export default function Level1() {
+//   return (
+//     <CatModelProvider>
+//       <NavigationControls>
+//         <Canvas
+//           shadows
+//           camera={{
+//             position: [0, 5, 10]
+//           }}>
+//           <color attach="background" args={['#FEF9F7']} />
+
+//           <Lights />
+
+//           <Suspense fallback={null}>
+//             <Physics>
+//               <Title />
+//               <World />
+//               <ShortcutManager />
+//               <SoundManager />
+//             </Physics>
+//           </Suspense>
+//         </Canvas>
+//         <GameInterface />
+//         <ShortcutManager />
+//       </NavigationControls>
+//     </CatModelProvider>
+//   );
+// }
--- src/routes/RoutesGame.jsx
+++ src/routes/RoutesGame.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Level1 from "../pages/level1/Level1";
 import Level2 from "../pages/level2/Level2";
+import Level3 from "../pages/level3/Level3";
 import Login from "../pages/login/Login";
 import Home from "../pages/main-view/Home";
 
@@ -12,8 +13,8 @@ export default function RoutesGames() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/level1" element={<Level1 />} />
                 <Route path="/level2" element={<Level2 />} />
-                {/* <Route path="/level3" element={<Level1 />} />
-                <Route path="/level4" element={<Level1 />} /> */}
+                <Route path="/level3" element={<Level3 />} />
+                {/* <Route path="/level4" element={<Level1 />} /> */}
             </Routes>
         </BrowserRouter>
     )
