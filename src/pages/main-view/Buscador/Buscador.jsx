c081481c638b4ed6d93725e00f2571f760a9e5b9
JoseManuel2005 <jose.palma@correounivalle.edu.co>
2024-06-17 22:35:39 -0500

    logo añadido al home

--- src/pages/main-view/Buscador/Buscador.jsx
+++ src/pages/main-view/Buscador/Buscador.jsx
@@ -2,16 +2,16 @@ import './Buscador.css'
 import { Link } from 'react-router-dom'
 
 export default function Buscador(){
+	const logo = '/assets/images/threedy-logo.svg';
+
 	return(
-		<div className="section-container">
+		<div className="title-container">
 			<div className="title-logo">
-				<h1 className='title-logo'>MeowVR</h1>
+				<h1 className='title-text'>MeowVR</h1>
 			</div>
 
-			<div className="login-container">
-				<div className="login-content">
-					{/* <Link to="./login" className="login-button">Acceder</Link> */}
-				</div>
+			<div className="logo-container">
+				<img src={logo} alt="imagen 1" className="image-opacity" />
 			</div>
 		</div>
 	);
